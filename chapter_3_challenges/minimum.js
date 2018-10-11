125fca7c28e90f6c4644539b5d6b2e1c1f92879b
Kyle <kylespresser@gmail.com>
2018-10-11 15:13:38 -0400

    chapter3 challenge1, author solution

--- chapter_3_challenges/minimum.js
+++ chapter_3_challenges/minimum.js
@@ -8,3 +8,9 @@ function minimum(num1, num2) {
 
 console.log(minimum(3,9))
 console.log(minimum(88,9))
+
+// Author solution
+// function min(a, b) {
+//   if (a < b) return a;
+//   else return b;
+// }
