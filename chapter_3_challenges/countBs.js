1ab41e61f6f899c25789c59ab2595f2dcd5d55ef
Kyle <kylespresser@gmail.com>
2018-10-11 15:45:52 -0400

    chapter3 challenge3 author solution

--- chapter_3_challenges/countBs.js
+++ chapter_3_challenges/countBs.js
@@ -25,3 +25,18 @@ const countBs = countChar('B')
 console.log(countBs('BBBB'))
 console.log(countBs('hhff'))
 console.log(countBs('bBnm'))
+
+// author solution
+// function countChar(string, ch) {
+//   let counted = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] == ch) {
+//       counted += 1;
+//     }
+//   }
+//   return counted;
+// }
+
+// function countBs(string) {
+//   return countChar(string, "B");
+// }
