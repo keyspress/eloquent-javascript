068e4c6be955ab881321ea441792e7364d4071aa
Kyle <kylespresser@gmail.com>
2018-10-13 13:42:46 -0400

    chapter 4 challenge 2 author solution, unsure why my solution does not work

--- chapter_4_challenges/reversing_an_array.js
+++ chapter_4_challenges/reversing_an_array.js
@@ -12,7 +12,8 @@ function reverseArrayInPlace (arr) {
     newArrValue.unshift(item)
   })
   console.log(newArrValue)
-  testArr = newArrValue
+  arr = newArrValue
+  return arr
 }
 
 testArr = [1,2,3,4,5]
@@ -21,3 +22,20 @@ testArr = [1,2,3,4,5]
 // console.log(testArr)
 reverseArrayInPlace(testArr)
 console.log(testArr)
+
+// function reverseArray(array) {
+//   let output = [];
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     output.push(array[i]);
+//   }
+//   return output;
+// }
+
+// function reverseArrayInPlace(array) {
+//   for (let i = 0; i < Math.floor(array.length / 2); i++) {
+//     let old = array[i];
+//     array[i] = array[array.length - 1 - i];
+//     array[array.length - 1 - i] = old;
+//   }
+//   return array;
+// }
