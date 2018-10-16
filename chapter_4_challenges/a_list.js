ef1a4f0382c4d1d26cb4bf0fa20218202b742ea8
Kyle <kylespresser@gmail.com>
2018-10-16 17:29:37 -0400

    working on a list challenge

--- chapter_4_challenges/a_list.js
+++ chapter_4_challenges/a_list.js
@@ -1,25 +1,37 @@
 function arrayToList (array) {
   let list = {}
-  array.forEach((item, i) => {
-    if(i === 0) {
-      list.value = item;
-      list.rest = {}
-    } else if(i === 1) {
-      list.rest.value = item;
-      list.rest.rest = {}
-    } else {
-      list.rest.rest.value = item;
-      list.rest.rest.rest = null
+  let listItem = {}
+  for (let i = array.length -1; i >= 0; i--){
+    if(i === array.length -1) {
+      listItem = {value: array[i], rest: null}
     }
-  });
-  return list
+  }
 }
+
+arrayToList()
+// function arrayToList (array) {
+//   let list = {}
+//   array.forEach((item, i) => {
+//     if(i === 0) {
+//       list.value = item;
+//       list.rest = {}
+//     } else if(i === 1) {
+//       list.rest.value = item;
+//       list.rest.rest = {}
+//     } else {
+//       list.rest.rest.value = item;
+//       list.rest.rest.rest = null
+//     }
+//   });
+//   return list
+// }
 
 function listToArray (list) {
   let arr = []
-  
+
 }
 
 const arr = [1,2,3]
 
-console.log(arrayToList(arr))
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
