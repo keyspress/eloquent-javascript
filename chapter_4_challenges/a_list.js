4ae478f1564ec46105847a928c09f3ddba427bd9
Kyle <kylespresser@gmail.com>
2018-10-17 10:02:06 -0400

    chapter 4 a list challenge my solution

--- chapter_4_challenges/a_list.js
+++ chapter_4_challenges/a_list.js
@@ -33,6 +33,12 @@ function prepend (value, rest) {
   return list
 }
 
+function nth (list, num) {
+  const arr = listToArray(list);
+  return arr[num]
+}
+
+
 const arr = [1,2,3]
 console.log(arrayToList(arr))
 console.log(arrayToList([10, 20]));
@@ -41,3 +47,5 @@ console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
