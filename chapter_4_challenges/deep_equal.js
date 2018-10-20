106939d0b20df7cfa6b0c1b5522356451d1344c0
Kyle <kylespresser@gmail.com>
2018-10-20 13:32:35 -0400

    chapter 4 deep equal challenge, my solution

--- chapter_4_challenges/deep_equal.js
+++ chapter_4_challenges/deep_equal.js
@@ -2,19 +2,21 @@ function deepEqual (item1, item2) {
   if((typeof item1 !== 'object') || (typeof item2 !== 'object')) {
     return item1 === item2
   } else {
-    let val1 = item1[Object.keys(item1)[0]];
-    let val2 = item2[Object.keys(item2)[0]];
-    // for (let item in item1) {
-    //   val1 = item;
-    //   console.log(val1)
-    //   break;
+    // for (i in item1) {
+    //   for(j in item2) {
+    //     return deepEqual(i, j)
+    //   }
     // }
-    // for (let item in item2) {
-    //   val2 = item;
-    //   break;
-    // }
-    console.log(val1, val2)
-    deepEqual(val1, val2)
+    let array1 = Object.values(item1)
+    let array2 = Object.values(item2)
+    for(let i = 0; i < array1.length; i++) {
+      return deepEqual(array1[i], array2[i])
+    }
+    // let val1 = array1[0];
+    // let val2 = array2[0];    
+    // return val1 === val2
+    // console.log(val1, val2)
+    // deepEqual(val1, val2)
   }
 }
 
