f1825430a6bfcdd0268ea47552eb11e2f8e00ba6
Kyle <kylespresser@gmail.com>
2018-10-20 13:34:05 -0400

    chapter 4 deep equal challenge, author solution

--- chapter_4_challenges/deep_equal.js
+++ chapter_4_challenges/deep_equal.js
@@ -2,21 +2,11 @@ function deepEqual (item1, item2) {
   if((typeof item1 !== 'object') || (typeof item2 !== 'object')) {
     return item1 === item2
   } else {
-    // for (i in item1) {
-    //   for(j in item2) {
-    //     return deepEqual(i, j)
-    //   }
-    // }
     let array1 = Object.values(item1)
     let array2 = Object.values(item2)
     for(let i = 0; i < array1.length; i++) {
       return deepEqual(array1[i], array2[i])
     }
-    // let val1 = array1[0];
-    // let val2 = array2[0];    
-    // return val1 === val2
-    // console.log(val1, val2)
-    // deepEqual(val1, val2)
   }
 }
 
@@ -28,3 +18,21 @@ console.log(deepEqual(obj, { here: 1, object: 2 }));
 // → false
 console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 // → true
+
+// author solution
+// function deepEqual(a, b) {
+//   if (a === b) return true;
+
+//   if (a == null || typeof a != "object" ||
+//     b == null || typeof b != "object") return false;
+
+//   let keysA = Object.keys(a), keysB = Object.keys(b);
+
+//   if (keysA.length != keysB.length) return false;
+
+//   for (let key of keysA) {
+//     if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+//   }
+
+//   return true;
+// }
