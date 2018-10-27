ad92849105be71d42389fd5c8876c7801e785418
Kyle <kylespresser@gmail.com>
2018-10-27 13:02:36 -0400

    chapter 5 end

--- chapter_5_challenges/every_and_then_some.js
+++ chapter_5_challenges/every_and_then_some.js
@@ -23,3 +23,18 @@ console.log(some([NaN, 3, 4], isNaN));
 // → true
 console.log(some([2, 3, 4], isNaN));
 // → false
+
+// author solution
+// function every(array, predicate) {
+//   for (var i = 0; i < array.length; i++) {
+//     if (!predicate(array[i])) return false;
+//   }
+//   return true;
+// }
+
+// function some(array, predicate) {
+//   for (var i = 0; i < array.length; i++) {
+//     if (predicate(array[i])) return true;
+//   }
+//   return false;
+// }
